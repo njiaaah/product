71f7cf1c6f07be5e9478c167a16c810c14d9b545
njiaaah <83307529+njiaaah@users.noreply.github.com>
2022-05-29 17:21:08 +0300

    small fixes

--- script.js
+++ script.js
@@ -232,7 +232,7 @@ document.addEventListener("DOMContentLoaded", function(event){
         let headOffset = document.getElementById('table-head').getBoundingClientRect()
 
         let y = this.scrollY
-        console.log(y)
+
         if (y > 300){
             document.getElementById('table-head').classList.add('sticky-head')
             document.getElementById('table-head').classList.remove('sticky-head-hidden')
@@ -242,19 +242,6 @@ document.addEventListener("DOMContentLoaded", function(event){
             document.getElementById('table-head').classList.add('sticky-head-hidden')
 
         }
-
-        // if (headOffset < 300){
-        //     document.getElementById('table-head').classList.add('sticky-head')
-        //     document.getElementById('table-head').classList.remove('sticky-head-hidden')
-
-        // } else {
-        //     document.getElementById('table-head').classList.remove('sticky-head')
-        //     document.getElementById('table-head').classList.add('sticky-head-hidden')
-
-        // }
     })
-
-
-
 })
 
